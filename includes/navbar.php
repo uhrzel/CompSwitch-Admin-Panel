0de62af7855651065e07f37b96536394a71cb855
Arzel John Zolina <Arzeljrz17@gmail.com>
2024-04-24 11:20:09 +0800

    done 2nd year 1st sem

--- includes/navbar.php
+++ includes/navbar.php
@@ -38,7 +38,7 @@
          <span>1st year 2nd sem</span></a>
      </li>
      <li class="nav-item">
-       <a class="nav-link" href="register.php">
+       <a class="nav-link" href="register2ndc1stsem.php">
          <i class="fas fa-fw fa-chart-area"></i>
          <span>2nd year 1st sem</span></a>
      </li>
