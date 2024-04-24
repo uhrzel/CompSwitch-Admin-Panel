6510902ff3ab40e9cbb52b2bd4fbb41abca18348
Arzel John Zolina <Arzeljrz17@gmail.com>
2024-04-24 12:26:40 +0800

    remove unecessary and done 4th year 1st sem

--- includes/navbar.php
+++ includes/navbar.php
@@ -58,7 +58,7 @@
          <span>3rd year 2nd sem</span></a>
      </li>
      <li class="nav-item">
-       <a class="nav-link" href="register.php">
+       <a class="nav-link" href="register4thc1stsem.php">
          <i class="fas fa-fw fa-chart-area"></i>
          <span>4th year 1st sem</span></a>
      </li>
@@ -110,16 +110,7 @@
          </button>
 
          <!-- Topbar Search -->
-         <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-           <div class="input-group">
-             <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-             <div class="input-group-append">
-               <button class="btn btn-primary" type="button">
-                 <i class="fas fa-search fa-sm"></i>
-               </button>
-             </div>
-           </div>
-         </form>
+
 
 
          <!-- Topbar Navbar -->
